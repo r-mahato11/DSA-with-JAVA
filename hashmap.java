fc5421ea3435778248eae82296f69bc508032681
Rohit-Mahato <83776656+Rohit-Mahato@users.noreply.github.com>
2024-02-22 00:21:13 +0530

    first commit

---
+++ hashmap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class hashmap{
+
+    public static void main(String[] args){
+        //Create
+        HashMap<string, Integer> hm = new HashMap<>();
+
+        //Insert
+        hm.put(key: "India", value:100);
+        hm.put(key: "China", value:150);
+        hm.put(key: "US", value:50);
+
+        System.out.println(hm);
+    }
+}
